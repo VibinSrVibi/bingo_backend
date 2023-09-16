664d98ac292a887092a55c051523742621117ffa
VibinSrVibi <vibinsrvibi97@gmail.com>
2023-09-16 21:14:42 +0530

    express and mongoose installed

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('welcome to bingo')
+const express = require('express');
+const app = express();
+const mongoose=require('mongoose');
+const port=process.env.PORT || 3000;
+
+//connect and create db
+try{
+    mongoose.connect('mongodb://localhost:27017/bingo',{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(res=>{
+        console.log('connected to db');
+    })
+}catch(e){
+    console.log('error ',e)
+}
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+  
+  app.listen(port,()=>{
+    console.log(`server is running at port ${port}`)
+})
